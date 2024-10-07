3fc9e03e2fe15bd3409fdfd2eb5155425f5aee8a
JC Franco <jfranco@esri.com>
2024-10-06 23:38:03 -0700

    apply fixes

--- test/unit/get-countries-data/parse-data/index.js
+++ test/unit/get-countries-data/parse-data/index.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 const parseData = require('../../../../lib/get-countries-data/parse-data');
 
 describe('.getCountriesData | .parseData', () => {
-  const { expectedCountries } = TestUtils.data;
+  const {expectedCountries} = TestUtils.data;
   let results = {};
 
   before(() => {
-    const dataPath = path.join(__dirname, './data.html')
+    const dataPath = path.join(__dirname, './data.html');
     const dataHtml = fs.readFileSync(dataPath, 'utf8');
     results = parseData(dataHtml);
   });
@@ -17,9 +17,9 @@ describe('.getCountriesData | .parseData', () => {
     expect(Object.keys(results).length).to.be.equal(249);
   });
 
-  Object.keys(expectedCountries).forEach(id => {
+  for (const id of Object.keys(expectedCountries)) {
     it(`should parse "${id}" correctly`, () => {
       expect(results[id]).to.be.eql(expectedCountries[id]);
     });
-  });
+  }
 });
